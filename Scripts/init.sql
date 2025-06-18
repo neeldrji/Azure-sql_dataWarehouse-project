9dea13a36fe5512c063a119d1c10735a85d3b14b
Neel Darji <neeldrji@gmail.com>
2025-06-18 16:50:25 -0400

    Create init.sql

---
+++ Scripts/init.sql
@@ -0,0 +1,19 @@
+
+
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
